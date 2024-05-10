cff467118652728539e3c25ac7b34fe65476bbe4
Paul Burgess <paul@burgess.io>
2024-05-10 17:51:18 -0400

    Start menu shaping

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,34 @@
 import { NavLink, Link } from "react-router-dom"  
 
 const Nav = () => {
+
+  const navToggle = document.querySelector('#nav-toggle');
+  
+  function onClick (){
+    alert('Clicked!')
+
+  }
+  
+  navToggle.addEventListener("click", onClick)
+
+
+
   return (
     <div>
-        <Link to="/">Home</Link>
-        <Link to="/">Our Barbers</Link>
-        <Link to="/">Services</Link>
-        <Link to="/">Book Appt</Link>
-    </div>
+      <div id="nav-toggle"> 
+        <img src="/images/Hamburger_icon.png" alt="" />
+      
+      </div>
 
+
+      <div className="nav-cont">
+          <Link to="/"><div className="nav-link">Home</div></Link>
+          <Link to="/"><div className="nav-link">Our Barbers</div></Link>
+          <Link to="/"><div className="nav-link">Services</div></Link>
+          <Link to="/"><div className="nav-link">Book Appt</div></Link>
+      </div>
+    </div>
   )
 }
 
-export default Nav
+export default Nav  
