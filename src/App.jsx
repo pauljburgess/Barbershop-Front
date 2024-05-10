48d66aff64fddaed73750b52099cadab0b278b6f
Paul Burgess <paul@burgess.io>
2024-05-10 16:17:19 -0400

    Start adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route, NavLink } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <div>
       <nav></nav>
-      <main></main>
+      <main>
+        <Routes>
+          <Route path='/' element={ <Home />}
+          />
+
+        </Routes>
+
+
+      </main>
       <footer></footer>
     </div>
   )
