752520ee90b4c70cbfbca42678329d6cc741c29d
Paul Burgess <paul@burgess.io>
2024-05-13 11:24:11 -0400

    Add barbers page

--- src/components/Barbers.jsx
+++ src/components/Barbers.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+
+
 
 const Barbers = () => {
+	
+	const [barbers, setBarbers] = useState([])
+
+	const fetchBarbers = async () => {
+		let response = await axios.get("https://mernsbbs-c43dae5036eb.herokuapp.com/barbers")
+		setBarbers(response.data)
+	}
+
+	useEffect(() => {
+		fetchBarbers()
+	}, [])
+
   return (
     <div className='content'>
         <h1>Our Team</h1>
+				{barbers.map(barber => (
+					<div>
+						<h3>{barber.name}</h3>
+						<p>{barber.bio}</p>
+					</div>
+				))}
     </div>
   )
 }
