b0db62d2b8d87d8dce808b9a305b5a6174275c0e
Paul Burgess <paul@burgess.io>
2024-05-17 15:46:32 -0400

    Start styling the Home Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <div className="header">
-        <img src="../images/logo.png" alt="" />
-        <h1 id="mern">Mern's Barbershop</h1>
+      {/* <<img src="../images/logo.png" alt="" /> */}
+      <h1 id="mern">Mern's Barbershop</h1>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <div className='content'>
-        <h1>Welcome!</h1>
+        <h1 id="welcome" className="roboto-flex-300">Welcome!</h1>
 
-        <p>Welcome to Mern's Barbershop! When you atep through our door, you're greeted by the rich scent of sandalwood and the buzz of clippers and animated conversations. Our skilled barbers are steeped in tradition and yet attuned to the latest trends. Whether you're after a sharp fade, a traditional shave, or simply seeking a moment of relaxation amidst the whirlwind of life, Mern's is where you belong. </p>
+        <p id="greeting">Welcome to Mern's Barbershop! When you atep through our door, you're greeted by the rich scent of sandalwood and the buzz of clippers and animated conversations. Our skilled barbers are steeped in tradition and yet attuned to the latest trends. Whether you're after a sharp fade, a traditional shave, or simply seeking a moment of relaxation amidst the whirlwind of life, Mern's is where you belong. </p>
         <Link to="/bookings/add"><button>Book Here!</button></Link>
     </div>
   )
