d56448ea1ca09473747602ce3a1c39899483eba4
Paul Burgess <paul@burgess.io>
2024-05-17 16:30:54 -0400

    Finish styling home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,10 @@ const Home = () => {
     <div className='content'>
         <h1 id="welcome" className="roboto-flex-300">Welcome!</h1>
 
-        <p id="greeting">Welcome to Mern's Barbershop! When you atep through our door, you're greeted by the rich scent of sandalwood and the buzz of clippers and animated conversations. Our skilled barbers are steeped in tradition and yet attuned to the latest trends. Whether you're after a sharp fade, a traditional shave, or simply seeking a moment of relaxation amidst the whirlwind of life, Mern's is where you belong. </p>
-        <Link to="/bookings/add"><button>Book Here!</button></Link>
+        <p id="greeting" >Browse a list of our barbers and services above, or go straight to booking your appointment if you know who and what you'd like. We look foward to seeing you!</p>
+
+
+        <Link to="/bookings/add"><button id="book-here" className="roboto-flex-300">Book now!</button></Link>
     </div>
   )
 }
