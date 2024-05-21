86ebc14e512a7afaca03b39aef034723b38f4dc4
Paul Burgess <paul@burgess.io>
2024-05-21 11:47:42 -0400

    Style admin barber page

--- src/components/Barbers.jsx
+++ src/components/Barbers.jsx
@@ -34,16 +34,17 @@ const Barbers = ({user}) => {
 
   return (
     <div className='content'>
-        <h1>Our Team</h1>
+        <h1 className='roboto-flex-300'>Our Team</h1>
 				{barbers.map(barber => (
 					<div key={barber._id}>
 						<h3>{barber.name}</h3>
 						<p>{barber.bio}</p>
 						{user ? 
 							<div>
-								<button onClick={() => whenClicked(barber._id)}>Update</button>
-								<button onClick={() => clicked(barber._id)}>Schedule</button>
-								<button onClick={() => onClick(barber._id)}>Delete</button>
+								<button className="schedule-button" onClick={() => clicked(barber._id)}>See {barber.name}'s Schedule</button>
+								<br />
+								<button className="update-button" onClick={() => whenClicked(barber._id)}>Update</button>
+								<button className="delete-button" onClick={() => onClick(barber._id)}>Delete</button>
 							</div>
 							
 						: ''}
