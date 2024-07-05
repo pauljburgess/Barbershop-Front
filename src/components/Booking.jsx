750e5cf20ed4075002c63cf108f8de1ad8138343
Paul Burgess <paul@burgess.io>
2024-07-05 10:05:12 -0400

    Make API call to get the service

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -21,10 +21,25 @@ const Booking = () => {
     fetchBooking()
   }, [])
 
+  const [service, setService] = useState()
+
+  const fetchService = async () => {
+    let response = await API.get(`/services/${booking.service[0]}`)
+    console.log(response.data.name)
+    setService(response.data.name)
+  }
+
+  useEffect(() => {
+    if (booking){
+      fetchService()
+    }
+  }, [booking])
+
   return (
-    <div>
+    <div className='content'>
       <h1>Booking for {booking.name}</h1>
-      
+      <h2>We're doing a {service}</h2>
+      <h3>{booking.phone}</h3>
     </div>
   )
 }
